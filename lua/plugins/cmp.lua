b68271ba82a855ec5d2f536466a0bbeebb72e9b7
zhaohiwei <zhaohiwei@szlongtu.com>
2026-08-24 10:53:28 +0800

    feat: 调整cmp补全用super-tab模式

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,11 +6,20 @@ return {
     event = 'InsertEnter',
     dependencies = { 'rafamadriz/friendly-snippets' },
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+                -- preset = 'default'
+                -- 直接声明使用 super-tab 预设
+                --https://yuanbao.tencent.com/chat/naQivTmsDa/0PrTyWp7oum
+                preset = 'super-tab'
+            },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
-      completion = { documentation = { auto_show = true } },
+      completion = { documentation = { auto_show = true },
+trigger = {
+      show_in_snippet = false  -- 避免在代码片段占位符里打字时疯狂弹菜单
+    }
+            },
       signature = { enabled = true },
     },
   },
